8076f971f1ffc7131d66d5c56f983e97573c7188
maya19808 <mayasriram19@gmail.com>
2026-01-30 18:01:36 -0500

    added sound, coded trash disappearance when pressing e

--- Assets/Scripts/pickUpStuff.cs
+++ Assets/Scripts/pickUpStuff.cs
@@ -1,23 +1,53 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class pickUpStuff : MonoBehaviour
 {
     public Transform player;
+    
     public Transform thing;
+    
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public bool isInteracting;
+    public InputAction interact;
+
+    
+    private void OnEnable()
+    {
+        interact.Enable();
+
+
+    }
+
+    private void OnDisable()
+    {
+        interact.Disable();
+    }
     void Start()
     {
-        public InputAction interact;
+        thing.gameObject.GetComponent<Renderer>().enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float distance = Vector3.Distance(player.position, thing.position);
-        Debug.Log(distance);
-        if (distance < 20)
+        //Debug.Log(distance);
+        isInteracting = interact.triggered;
+        if (distance < 20 && isInteracting)
         {
-            Debug.Log("hello");
+            //TODO: Create system that will remove object from game
+               Debug.Log("Object interacted with");
+               thing.gameObject.GetComponent<AudioSource>().Play();
+               thing.gameObject.GetComponent<Renderer>().enabled = false;
+               //Disables mesh of objects making it disappear (YIPPEEEE)
+            
         }
+
+        
+    
     }
 }
